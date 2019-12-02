f8f6d2162fd583f70909df5c3498ea57e956b024
yangsen <ysen173711@163.com>
2019-12-02 22:31:35 +0800

    '解决ssr的bug'

--- src/entry-client.js
+++ src/entry-client.js
@@ -3,7 +3,5 @@ import createApp from './create-app'
 const { app, router, store } = createApp()
 
 router.onReady(() => {
-  const root = document.createElement('div')
-  document.body.appendChild(root)
-  app.$mount(root)
+  app.$mount("#root")
 })
